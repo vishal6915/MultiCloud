67ac1bf4a64c4502111d6af18704e1a1e93e8c9b
shaisthajabeen610 <67311615+shaisthajabeen610@users.noreply.github.com>
2022-07-29 12:18:50 +0530

    only pricing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PricingComponent } from './pricing/pricing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',pathMatch: 'full' ,redirectTo:'/pricing'
+  },
+  {path: 'pricing',component:PricingComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PricingComponent } from './pricing/pricing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PricingComponent
   ],
   imports: [
     BrowserModule,
