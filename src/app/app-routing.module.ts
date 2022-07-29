72eec65f0d5e0081a3602c98b855f6dec907bd06
shaisthajabeen610 <67311615+shaisthajabeen610@users.noreply.github.com>
2022-07-29 16:00:21 +0530

    separate app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PricingComponent } from './pricing/pricing.component';
+//import { PricingComponent } from './pricing/pricing.component';
 
 const routes: Routes = [
-  {
-    path:'',pathMatch: 'full' ,redirectTo:'/pricing'
-  },
-  {path: 'pricing',component:PricingComponent}
+  // {
+  //   path:'',pathMatch: 'full' ,redirectTo:'/pricing'
+  // },
+  // {path: 'pricing',component:PricingComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PricingComponent } from './pricing/pricing.component';
+//import { PricingComponent } from './pricing/pricing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PricingComponent
+   // PricingComponent
   ],
   imports: [
     BrowserModule,
