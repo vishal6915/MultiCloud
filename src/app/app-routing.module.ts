a4b74fb13d0bf7e06970449e1c49c80344631382
shaisthajabeen610 <67311615+shaisthajabeen610@users.noreply.github.com>
2022-08-01 17:01:02 +0530

    404 file removed from dist

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PricingComponent } from './pricing/pricing.component';
 //import { PricingComponent } from './pricing/pricing.component';
 
 const routes: Routes = [
-  // {
-  //   path:'',pathMatch: 'full' ,redirectTo:'/pricing'
-  // },
-  // {path: 'pricing',component:PricingComponent}
+  {
+    path:'',pathMatch: 'full' ,redirectTo:'/pricing'
+  },
+  {path: 'pricing',component:PricingComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, { useHash: false, onSameUrlNavigation: 'reload' })  
+    
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PricingComponent } from './pricing/pricing.component';
 //import { PricingComponent } from './pricing/pricing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    PricingComponent,
    // PricingComponent
   ],
   imports: [
